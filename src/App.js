bb51ddfe15a1de1576f957cbd6229cd5c27b954d
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-06-27 00:12:24 +0400

    Createnavigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return <div>Hello</div>;
-}
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import MyProfile from './components/MyProfile';
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Rockets />} />
+          <Route path="missions" element={<Missions />} />
+          <Route path="my-profile" element={<MyProfile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 export default App;
