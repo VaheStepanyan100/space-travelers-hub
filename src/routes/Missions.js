978562a61797a63869d36ac732ceb47aed5f8d9e
zbahati <bahatizirimwabagabo@gmai.com>
2023-06-28 07:40:05 +0200

    Ft/FetchMission
    - Add Missions.js file into routes that renders missionsList to the screen by using props
    [# Finishes ..]

--- src/routes/Missions.js
+++ src/routes/Missions.js
@@ -1,3 +1,45 @@
-const Missions = () => <div>Mission</div>;
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMissions } from '../redux/missions/missionsSlice';
+import { selectMissions } from '../redux/store';
+import MissionList from '../components/MissionList';
+import { GET_MISSIONS } from '../redux/api';
 
-export default Missions;
+const Mission = () => {
+  const {
+    missionItems, loading, error, errMsg,
+  } = useSelector(selectMissions);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (missionItems.length > 0) return;
+    dispatch(getMissions({ url: GET_MISSIONS }));
+  }, [dispatch, missionItems.length]);
+
+  if (loading) {
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>
+        <p>
+          Oops! an error occurred. Please try again.
+          {errMsg}
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <MissionList missions={missionItems} />
+    </div>
+  );
+};
+
+export default Mission;
